6d1fe8ca24a48ae945500f790aced1da2574b025
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2024-09-30 15:28:53 +0300

    replaced logo and ico

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	"github.com/rostis232/complicode/internal/pkg/app"
+	"fmt"
 	"os"
+
+	"github.com/rostis232/complicode/internal/pkg/app"
 
 	"github.com/labstack/gommon/log"
 )
 
 func main() {
+	fmt.Println("Starting app...")
 	token := os.Getenv("TOKEN")
 	key := os.Getenv("KEY")
+	port := os.Getenv("PORT")
+	if token == "" || key == "" || port == "" {
+		log.Panicf("Some env are not set:\nTOKEN: %s\nKEY: %s\nPORT: %s", token, key, port)
+	}
+	log.Infof("PORT: %s", port)
+	
 	a, err := app.NewApp(token, key)
 	if err != nil {
 		log.Panic(err)
 	}
-	port := os.Getenv("PORT")
-	log.Infof("PORT: %s", port)
+
 	if err := a.Run(port); err != nil {
 		log.Panic(err)
 	}
